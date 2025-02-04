4afadb7d4b0d494f6a2eb500977a995b956f52dc
Jeremih333 <carreljeremih@gmail.com>
2025-02-05 00:33:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000; // Используйте порт из переменной окружения
+const PORT = process.env.PORT || 10000; // Используйте порт из переменной окружения
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на http://localhost:${PORT}`);
