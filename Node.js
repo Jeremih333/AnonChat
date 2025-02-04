ff3328fe347d7c4430d325978116892284a9a708
Jeremih333 <carreljeremih@gmail.com>
2025-02-04 17:05:45 +0300

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
+// server.js
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Define a route for the homepage
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
