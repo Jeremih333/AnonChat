9054ad3f3fc397905a6d6f97c971d94b9f2c4318
Jeremih333 <carreljeremih@gmail.com>
2025-02-05 16:22:37 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000; // Используйте порт из переменной окружения
-
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на http://localhost:${PORT}`);
-});
